98e4efc99d4d7feb1a39fa5c73a191d5d82213a6
Karyna <88458417+DevGirl20@users.noreply.github.com>
2023-02-17 18:36:26 +0200

    1_js_update
    js

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//1.1
+//написали можливі способи підключення
+//1.2
+let nameSurname = '';
+let userName = '';
+let name1 = '';
+let name$ = '';
+let name_Surname = '';
+
+//let 1name_Surname = '';
+//let имя = '';
+//let let = '';
+//let name-surname = ''; (под вопросм в одних статьях пишут, что можно, в других - не поддерживается "-" )
+
+//1.3
+
+/* ... */
+
+//...
+
+//
+
+/**
+* 
+*
+*
+*
+*/
+
+//1.4
+//loverCamelCase
+//CamelCase
+//snake_case
+
+//2.1
+
+let name = prompt("Напиши свое имя и фамилию");
+alert(`Привет ${name}`);
+
+//2.2
+const yearNow = 2023;
+let whatYourAge = prompt("Напиши какого ты года рождения");
+alert(`Тебе ${yearNow - whatYourAge}`);
+
+
+//2.3
+let length = prompt("Я помогу посчитать периметр квадрата - напиши длину стороны квадрата");
+alert(`периметр твоего квадрата -  ${4 * length}`);
+
+
